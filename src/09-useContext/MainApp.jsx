f73f5eefcdcbe814aaf66d14b17921a7bcdab5e9
ValentinZurita <67253631+ValentinZurita@users.noreply.github.com>
2025-01-09 10:07:00 -0600

    Agregando Navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { AboutPage } from './AboutPage'
 import { HomePage } from './HomePage'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navbar } from './Navbar'
+import { LoginPage } from './LoginPage'
 
 export const MainApp = () => {
   return (
     <>
       <div>MainApp</div>
+      <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
