b26aa1202454d2573891a9dc2ac4e19556df3a76
ValentinZurita <67253631+ValentinZurita@users.noreply.github.com>
2025-01-10 17:17:58 -0600

    Agregando el Navbar

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 export const Navbar = () => {
   return (
-    <>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/login">Login</Link>
-    </>
+    // <>
+    // <Link to="/">Home</Link>
+    // <Link to="/about">About</Link>
+    // <Link to="/login">Login</Link>
+    // </>
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          UseContext
+        </Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active' : ''}`
+              }
+              to="/"
+            >
+              HOME
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active' : ''}`
+              }
+              to="/about"
+            >
+              ABOUT
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active' : ''}`
+              }
+              to="/login"
+            >
+              LOGIN
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
